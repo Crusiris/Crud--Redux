c924846392d8a0e8a13f586fd5000df81a912072
Crusiris <cruzsirys@gmail.com>
2020-06-24 17:34:13 -0400

    Obteniendo state de error y loading y mostrando mensajes al usuario segun valores

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,8 +3,8 @@ import Product from './Product';
 import { useDispatch, useSelector } from 'react-redux';
 //Importando actions [FUNCION QUE CREA EL PRODUCTO] de Redux 
 import { getProductsAction } from '../../actions/productsAction';
-
-import { Grid, Typography } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import { Grid, Typography, LinearProgress } from '@material-ui/core';
 import useStyles from './style';
 
 const Products = () => {
@@ -23,18 +23,22 @@ const Products = () => {
 
     //Obteniendo datos del state con useSelector
     const products = useSelector(state => state.products.products);
-    
+    const error = useSelector(state => state.products.error);
+    const load = useSelector(state => state.products.loading);
+
     return(
         <Grid container direction="row" justify="center">
          
-        <div className={classes.offset} />
+         <div className={classes.offset} />
                 
             <Grid item xs={12}  md={10} lg={8} className={classes.containertable}>
 
                 <Typography variant="h5" align="center" gutterBottom className={classes.title}>
                     Listado de producto
                 </Typography>
-
+                
+                {error ?( <Alert severity="error">Ha ocurrido un error</Alert>): null}
+                {load ? (  <LinearProgress />):null}
 
                 <table className="table" >
                     <thead>
